535f11cb05fa72225919ad15eb7fd1ac410df7e9
Brad Sickles <brad.sickles@gmail.com>
2016-02-06 08:54:06 -0500

    Fixing sourcemaps.
    Removing deprecated gulp-symlink.
    Adding `gulp help`.

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,39 +1,54 @@
-var fs = require('fs'),
+var gulp = require('gulp'),
+    fs = require('fs'),
+    taskListing = require('gulp-task-listing'),
     typings = require('bower-typings'),
     allTypings = typings(),
     name = '<%= name %>',
     meta = {
         name: name,
-        src: [
-            'typings/*.d.ts',
-            'src/_version.ts',
-            'src/**/*.ts'
-        ].concat(typings({includeSelf: false})),
+        getSrc: () => {
+            return [
+                'typings/*.d.ts',
+                'src/_version.ts',
+                'src/**/*.ts'
+            ].concat(typings({includeSelf: false}))
+        },
+        getScaffold: (name) => {
+            return meta.scaffolds.filter(function (scaffold) {
+                return scaffold.name === name;
+            })[0];
+        },
         scaffolds: [
             {
                 name: 'test',
                 symdirs: ['dist', 'src', 'themes'],
-                src: [
-                    'typings/*.d.ts',
-                    'test/**/*.ts',
-                    '!test/lib/**/*.ts',
-                    'dist/' + name + '.d.ts'
-                ].concat(allTypings)
+                getSrc: () => {
+                    return [
+                        'typings/*.d.ts',
+                        'test/**/*.ts',
+                        '!test/lib/**/*.ts',
+                        `dist/${name}.d.ts`
+                    ].concat(allTypings);
+                }
             },
             {
                 name: 'testsite',
                 ignore: 'lib/qunit',
                 port: 8001,
                 symdirs: ['dist', 'src', 'themes'],
-                src: [
-                    'typings/*.d.ts',
-                    'testsite/**/*.ts',
-                    '!testsite/lib/**/*.ts',
-                    'dist/' + name + '.d.ts'
-                ].concat(allTypings)
+                getSrc: () => {
+                    return [
+                        'typings/*.d.ts',
+                        'testsite/**/*.ts',
+                        '!testsite/lib/**/*.ts',
+                        `dist/${name}.d.ts`
+                    ].concat(allTypings);
+                }
             }
         ]
     };
+
+gulp.task('help', taskListing);
 
 fs.readdirSync('./gulp')
     .forEach(function (file) {
